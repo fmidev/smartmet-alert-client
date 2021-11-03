1170bb1666b3f24811979f3486ca118f373490b9
Matti Pulakka <matti@pulakka.net>
2021-11-03 09:41:17 +0200

    SOL-13334 Add a button role to region toggle

--- src/components/Region.vue
+++ src/components/Region.vue
@@ -9,7 +9,7 @@
                 {{ regionName }}
               </span>
             </div>
-            <b-button block v-b-toggle="identifier" variant="info" class="current-warning-toggle" :aria-label="ariaButton" />
+            <b-button block v-b-toggle="identifier" variant="info" class="current-warning-toggle" :aria-label="ariaButton" role="button" />
         </b-card-header>
         <b-collapse
                 :id=identifier
