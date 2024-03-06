b2e1693461f6220623cabf5494a1f11412b4fd2d
Matti Pulakka <matti.pulakka@fmi.fi>
2024-03-06 15:01:57 +0200

    [SOL-17260] Restore custom element attributes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import App from './App.vue'
 class SmartMetAlertClient extends HTMLElement {
   constructor() {
     super()
-    const app = createApp(App)
+    const app = createApp(
+      App,
+      [...this.attributes].reduce((newObject, item) => {
+        newObject[item.nodeName] = item.nodeValue
+        return newObject
+      }, {})
+    )
     app.mount(this.attachShadow({ mode: 'open' }))
   }
 }
